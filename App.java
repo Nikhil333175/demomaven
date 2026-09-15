240be4db4752a73921a927e67f58b7b0d06fc887
Bubu <163994129+bhuvan-raj@users.noreply.github.com>
2026-09-15 11:39:25 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello from Jenkins Freestyle Project!");
     }
 
     public static int add(int a, int b) {
