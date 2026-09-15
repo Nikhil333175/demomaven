2fcbf66c3c96811df0fd370db4397ba66a35585f
Nikhil Murali <nikhilmurali175@gmail.com>
2026-09-15 11:53:36 +0530

    Refactor App class to use getMessage method
    Refactored main method to use getMessage method and updated message.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,13 @@
-package com.example;
-
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Freestyle Project!");
-    }
+```
+public static String getMessage() {
+    return "Hello from Jenkins Maven Project!";
+}
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
+public static void main(String[] args) {
+    System.out.println(getMessage());
+}
+```
+
 }
